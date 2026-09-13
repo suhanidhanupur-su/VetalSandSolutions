d2325d48feb01e373d53b6c2224b6e8e7dff6291
Suhani kumari <suhanidhanupur@gmail.com>
2026-09-13 11:44:31 +0530

    Added

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class CorePageViewsTests(TestCase):
+    def test_home_page_loads(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Vetal Sand Solutions')
+        self.assertContains(response, 'Request a Quote')
+        self.assertContains(response, 'Home')
+
+    def test_about_page_loads(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'About Vetal Sand Solutions')
+        self.assertContains(response, 'Vetal Sand Solutions')
+        self.assertContains(response, 'About')
+        self.assertContains(response, 'Premium quality silica sand solutions for demanding industrial applications.')
